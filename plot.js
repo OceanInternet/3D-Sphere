1ee6c4291b31eeec7a97d687a5af177e5acbb135
Andy Weir <OceanInternet@users.noreply.github.com>
2014-07-31 19:54:02 +0100

    Create plot.js

---
+++ plot.js
@@ -0,0 +1,15 @@
+var radius = 400,
+    rows   = 16,
+    cols   = rows / 2,
+    top    = 0,
+    col    = 0;
+
+for(bottom=1;bottom<=rows;bottom++) {
+
+    var yAngle = 180 * bottom / rows;
+
+    x = radius * cos(a)
+    y = radius * sin(a)
+    
+    top = bottom;
+}
